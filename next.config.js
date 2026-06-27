bb1a320cf320f05e4c5553c28ae2cb5aac637c0b
Safin313 <safin313@example.com>
2026-06-27 19:07:20 +0600

    fix: reduce build memory usage for vercel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "i.ibb.co", "res.cloudinary.com"],
   },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
+  swcMinify: false,
 };
 
 module.exports = nextConfig;
